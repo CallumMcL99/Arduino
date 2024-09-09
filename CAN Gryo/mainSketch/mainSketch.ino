b696dfbff915ec126a2673bb7b1617bd39bcf3fe
CallumMcLean <callum.mclean@vocean.co.uk>
2024-09-09 16:05:59 +0100

    Code cleanup.

--- CAN Gryo/mainSketch/mainSketch.ino
+++ CAN Gryo/mainSketch/mainSketch.ino
@@ -3,34 +3,28 @@
 #include "Wire.h"
 #include <SPI.h>
 
+// Gyro
 typedef DFRobot_BNO055_IIC    BNO;    // ******** use abbreviations instead of full names ********
 BNO   bno(&Wire, 0x28);    // input TwoWire interface and IIC address
 
+//CAN-BUS
 const int SPI_CS_PIN = 9;
 DFRobot_MCP2515 CAN(SPI_CS_PIN);
 
-char startChar = '$';
-char endChar = '#';
-char seperatorChar = ' ';
 uint32_t canID = 0x00FFFF0C;
-
-const int size = 10;
-unsigned char msgOne[size] =  {0x24, 0x00, 0x00, 0xFF, 0xFF, 0xAA, 0xBB, 0xCC};
-
-
+const int size = 8;
+unsigned char msgOne[size] =  {0x24, 0x00, 0x00, 0xFF, 0xFF, 0xAA, 0xBB, 0xDD};
 
 void setup()
 {
-  Serial.println("Board started.");
+  //Serial.println("Board started.");
 
   Serial.begin(57600);
   bno.reset();
 
-  // Wait until the gyro is connected.
   while(bno.begin() != BNO::eStatusOK) {
-    Serial.println("Gryo failed.");
-    //PrintMessage("0");
-    delay(2000);
+    //Serial.println("Gryo failed.");
+    delay(500);
   }
 
   Serial.println("Gryo inited.");
@@ -48,89 +42,56 @@ void setup()
       }
   }while(!canInit);
 
-  Serial.println("CAN shield inited.");
-  Serial.println("Board initialised. Loop starting.");
+  //Serial.println("CAN shield inited. Board initialised. Loop starting.");
 }
 
 void loop()
 {
-  //BNO::sEulAnalog_t   sEul;
-  //sEul = bno.getEul();
+  BNO::sEulAnalog_t   sEul;
+  sEul = bno.getEul();
   //PrintMessage(seperatorChar + "1" + seperatorChar + String(sEul.pitch, 3) + seperatorChar + String(sEul.roll, 3) + seperatorChar + String(sEul.head, 3));
 
   if(CAN_MSGAVAIL == CAN.checkReceive()) {
     ReadMessages();
   }
   else{
-    PrintMessage("");
+    SendCanMessage(msgOne);
   }
 
-  if (CAN.checkError() == CAN_CTRLERROR)
-  {
-    Serial.println("ERROR");
-  }
+  // if (CAN.checkError() == CAN_CTRLERROR)
+  // {
+  //   Serial.println("ERROR");
+  // }
 
   delay(100);
 }
 
-void PrintMessage(String message)
-{
-    SendCanMessage(msgOne, "1");
-}
+void SendCanMessage(unsigned char message[]){
+  Serial.println("Sending CAN");
+  CAN.sendMsgBuf(0x00FFFF0C, 1, size, message);
 
-void SendCanMessage(unsigned char message[], String name){
-  // FFFF07 - 16776967
-  Serial.print("Snd msg: ");
-  for (int i = 0; i < size; i++)
-  {
-    Serial.print(message[i]);
-    Serial.print(", ");
-  }
-  Serial.println("");
-
-  if (MCP2515_OK == CAN.sendMsgBuf(0x00FFFF0C, 1, size, message)){
-    Serial.println("Sent CAN " + name);
-  }
-  else{
-    Serial.println("Failed to send CAN " + name);
-  }
-}
-
-void ReadMessages(){
-  // if(CAN_MSGAVAIL == CAN.checkReceive())            // check if data coming
-  // {
-    unsigned char len = 0;
-    unsigned char buf[32];
-
-    uint32_t id = CAN.getCanId();
-    Serial.print(id);
-    Serial.print("\t\t");
-
-    CAN.readMsgBuf(&len, buf);    // read data,  len: data length, buf: data buf
-
-    for(int i = 0; i < len; i++)    // print the data
-    {
-        Serial.print(buf[i]);
-        Serial.print("\t");
-    }
-    Serial.println();
+  // if (MCP2515_OK == CAN.sendMsgBuf(0x00FFFF0C, 1, size, message)){
+  //   Serial.println("Sent CAN ");
+  // }
+  // else{
+  //   Serial.println("Failed to send CAN ");
   // }
 }
 
-void PrintAvailableCanPorts(){
-  for (int i = 0; i < 100; i++)
-  {
-    Serial.println("Trying " + String(i));
-    DFRobot_MCP2515 CANX(i);
-    if(CAN_OK != CANX.begin(CAN_500KBPS))                   // init can bus : baudrate = 500k
-    {
-        Serial.println("DFROBOT's CAN BUS Shield init fail");
-        delay(100);
-    }
-    else{
-        Serial.println("CAN-Bus shield inited");
-    }
-  }
+void ReadMessages(){
+    unsigned char len = 0;
+    unsigned char buf[32];
+
+    // uint32_t id = CAN.getCanId();
+    // Serial.print(id);
+    // Serial.print("\t\t");
+    Serial.println("Recieving CAN");
+    CAN.readMsgBuf(&len, buf);
+
+    // for(int i = 0; i < len; i++)
+    // {
+    //     Serial.print(buf[i]);
+    //     Serial.print("\t");
+    // }
+    // Serial.println();
 }
-
-
